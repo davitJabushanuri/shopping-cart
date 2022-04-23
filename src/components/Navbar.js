fd7e485c3e5c7f2f278ed10841fa6ddecf8ee442
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-04-23 20:32:03 +0400

    setup navbar, add icons from fontawesome

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+	faHome,
+	faBagShopping,
+	faHeart,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
 	return (
 		<nav>
 			<div className='logo'>Logo</div>
 			<div className='navbar'>
-				<Link to='/'>Home</Link>
-				<Link to='/products'>Products</Link>
-				<Link to='/about'>About</Link>
-				<Link to='/contact'>Contact</Link>
-				<Link to='/favorites'>Favorites</Link>
-				<Link to='/shopping-cart'>Shopping Cart</Link>
+				<Link className='link' to='/'>
+					<FontAwesomeIcon icon={faHome} />
+				</Link>
+				<Link className='link' to='/products'>
+					Products
+				</Link>
+				<Link className='link' to='/about'>
+					About
+				</Link>
+				<Link className='link' to='/contact'>
+					Contact
+				</Link>
+			</div>
+
+			<div className='cart'>
+				<Link className='link' to='/favorites'>
+					<FontAwesomeIcon icon={faHeart} />
+				</Link>
+				<Link className='link' to='/shopping-cart'>
+					<FontAwesomeIcon icon={faBagShopping} />
+				</Link>
 			</div>
 		</nav>
 	)
