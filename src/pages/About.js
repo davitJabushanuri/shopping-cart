1137a0840ea86f8da340294f3d32327b4b8788fa
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-04-23 22:56:09 +0400

    add hero section on Home page, add dumy text on About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,92 @@
 import React from 'react'
 
 const About = () => {
-	return <div>About</div>
+	return (
+		<div className='about'>
+			<div className='delivery'>
+				<h1>Taking Delivery</h1>
+				<p>
+					Taking delivery of your new Tesla is a quick and easy experience. With
+					Tesla Tutorial videos available right on your touchscreen, you can
+					learn about the features and functions of your Tesla from the driver’s
+					seat. After placing your order, sign in to your Tesla Account to
+					complete the required delivery tasks and prepare for delivery. The
+					latest updates on delivery timing, VIN assignment and appointment
+					details will be available in your Tesla Account. Depending on location
+					and eligibility, your Tesla will be available for pickup at a delivery
+					center or will be delivered to your location. Please note that
+					delivery dates may change based on many factors including logistics
+					and completion of your pre-delivery tasks.
+				</p>
+
+				<h1>Express Delivery</h1>
+
+				<p>
+					Take delivery at a Tesla location. After arriving at your delivery
+					location, locate and access your Tesla by using the Tesla app. We will
+					provide your required registration documents, along with temporary
+					registration and plates, depending on your state. Express Delivery
+					Steps Sign available agreements, submit final payment and accept
+					delivery in your Tesla app. Unlock and access your Tesla through the
+					Tesla app. Complete your registration documents (subject to state
+					requirements). Explore the features at your own pace with the in-car
+					Tesla Tutorials and Owner's Manual. Watch the Tesla Tutorial videos
+					anytime on your touchscreen, in the Tesla app or on Tesla Support.
+					Express Delivery is available at all Tesla delivery centers. Some
+					states may vary based on signatory requirements.
+				</p>
+
+				<h1>Tesla Direct</h1>
+
+				<p>
+					Tesla will deliver your car to an eligible delivery address. Your
+					Tesla is delivered directly to your provided delivery address. Once
+					your Tesla has arrived, sign any remaining documents and drive away
+					within minutes. Tesla Direct is only available in certain areas to
+					customers within an approved distance from their local Tesla delivery
+					center. Not all customers will qualify. Tesla Direct Steps Once your
+					car arrives, sign available agreements, submit payment and accept
+					delivery in your Tesla app. Unlock and access your Tesla through the
+					Tesla app. Follow the provided registration documents (subject to
+					state requirements) and mail them back in a pre-paid envelope within
+					24 hours. Explore the features at your own pace with the in-car Tesla
+					Tutorials and Owner's Manual. Watch the Tesla Tutorial videos anytime
+					on your touchscreen, in the Tesla app or on Tesla Support.
+				</p>
+
+				<h1>Carrier Direct</h1>
+
+				<p>
+					Take delivery from an independent carrier. This fee-based option is
+					available for customers who prefer to have their car delivered, and
+					who live more than 220 miles from the nearest Tesla delivery location.
+					These deliveries are performed by independent carriers for a fee,
+					based on your final delivery location. For faster delivery, we
+					recommend taking delivery at a Tesla delivery location. Talk to your
+					Tesla Advisor if you would like to add this delivery option to your
+					order. Your delivery date will come directly from the carrier service.
+					Tesla will provide a delivery date, which will be confirmed by the
+					independent carrier once your car is in transit. Before Shipment Sign
+					and return any applicable financing, leasing or trade-in documents.
+					Tesla will coordinate shipment upon receipt of payment and signed
+					documents. Sign available agreements and submit payment in your Tesla
+					app. If you have a trade-in, prepare it for pickup. You will receive a
+					delivery date from Tesla, which will be confirmed by the independent
+					carrier once your car is in transit. Delivery Day You will receive a
+					delivery timing update from the independent carrier. Accept delivery
+					of your Tesla in your Tesla app. Sign and mail any remaining documents
+					in the car via a pre-paid shipping envelope within 24 hours. Note: For
+					Hawaii-outer-island deliveries, we will coordinate shipping to your
+					preferred Hawaiian island through locally-approved shipping companies.
+					The shipping company will deliver your Tesla directly to your island,
+					and will connect with you directly for payment. Prices are set by the
+					shipping provider and may vary. Prior to receiving your car, we
+					recommend watching the Tesla Tutorial videos and downloading the Tesla
+					app.
+				</p>
+			</div>
+		</div>
+	)
 }
 
 export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-	return <div>Home</div>
+	return (
+		<div className='home'>
+			<div className='hero'>
+				<div className='heading'>
+					<h1>Model 3</h1>
+					<p>
+						Order Online for <Link to='/about'>Touchless Delivery</Link>
+					</p>
+				</div>
+				<div className='action-buttons'>
+					<button className='call-to-action black'>CUSTOM ORDER</button>
+					<button className='call-to-action'>
+						<Link to='/shop'>EXISTING INVENTORY</Link>
+					</button>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Home
