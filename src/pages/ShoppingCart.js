3f0735c16642041defb3d6cc53dd9359e7981adb
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-04-25 05:23:57 +0400

    add style to ShoppinCart page

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,15 +1,39 @@
 import React, { useContext } from 'react'
 import cartContext from '../Helpers/cartContext'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-const ShoppingCart = () => {
+const ShoppingCart = ({ quantity }) => {
 	const { cart, setCart } = useContext(cartContext)
 
 	return (
-		<div>
+		<div className='shopping-cart'>
 			{!cart
 				? 'Your cart is empty'
 				: cart.map(item => {
-						return <li>{item.model}</li>
+						return (
+							<div key={item.id} className='cart-item'>
+								<div className='cart-img-container'>
+									<img src={item.img} alt='' className='cart-img' />
+								</div>
+								<div className='cart-info'>
+									<div className='cart-model'>
+										<h1>{item.model}</h1>
+									</div>
+									<p className='cart-price'>${item.price}</p>
+									<p>quantity {quantity ? quantity : `1`}</p>
+									<div className='delivery'>
+										<span className='delivery-heading'>Delivery</span>
+										<span className='est-delivery'>
+											Est. delivery: Apr 28 - May 2
+										</span>
+									</div>
+								</div>
+								<button className='delete'>
+									<FontAwesomeIcon className='delete-icon' icon={faTrash} />
+								</button>
+							</div>
+						)
 				  })}
 		</div>
 	)
