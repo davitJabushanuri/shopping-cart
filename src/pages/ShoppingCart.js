0944bd529f06b0f986a619d2b3631fc3e2b71dab
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-04-26 00:08:39 +0400

    add checkout tab that show order total

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,48 +1,39 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import cartContext from '../Helpers/cartContext'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+import Checkout from '../components/Checkout'
+import Cart from '../components/Cart'
 
 const ShoppingCart = ({ quantity = 1 }) => {
 	const { cart, setCart } = useContext(cartContext)
+	const [total, setTotal] = useState(0)
 
 	const handleDelete = carId => {
 		setCart(x => x.filter(car => car.id !== carId))
-		console.log(cart)
 	}
 
+	const getTotal = arr => {
+		let x = 0
+		for (let i = 0; i < arr.length; i++) {
+			x += arr[i].price
+		}
+
+		return x
+	}
+
+	useEffect(() => {
+		if (cart.length > 0) {
+			setTotal(getTotal(cart))
+			console.log(total)
+		}
+	}, [cart])
+
 	return (
-		<div className='shopping-cart'>
-			{!cart
-				? 'Your cart is empty'
-				: cart.map(item => {
-						return (
-							<div key={item.id} className='cart-item'>
-								<div className='cart-img-container'>
-									<img src={item.img} alt='' className='cart-img' />
-								</div>
-								<div className='cart-info'>
-									<div className='cart-model'>
-										<h1>{item.model}</h1>
-									</div>
-									<p className='cart-price'>${item.price}</p>
-									<p>quantity {quantity ? quantity : `1`}</p>
-									<div className='delivery'>
-										<span className='delivery-heading'>Delivery</span>
-										<span className='est-delivery'>
-											Est. delivery: Apr 28 - May 2
-										</span>
-									</div>
-								</div>
-								<button
-									onClick={() => handleDelete(item.id)}
-									className='delete'
-								>
-									<FontAwesomeIcon className='delete-icon' icon={faTrash} />
-								</button>
-							</div>
-						)
-				  })}
+		<div className='cart-container'>
+			{cart.length > 0 && (
+				<Cart cart={cart} quantity={quantity} handleDelete={handleDelete} />
+			)}
+			{cart.length > 0 && <Checkout total={total} />}
 		</div>
 	)
 }
